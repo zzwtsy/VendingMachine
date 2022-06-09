c5c66cdffbb547ab87bc861936b30dd2d1ddd544
zzwtsy <58852496+zzwtsy@users.noreply.github.com>
2022-06-09 16:51:24 +0800

    添加后台管理界面

---
+++ src/hwk1/GUI/Menu.java
@@ -0,0 +1,26 @@
+package hwk1.GUI;
+
+import javax.swing.*;
+
+public class Menu {
+    private JFrame frame;
+    private JPanel root;
+    private JButton maintainSystemButton;
+    private JButton sellSystemButton;
+    private JButton initSystemButton;
+    private JButton exitSystemButton;
+
+    public Menu() {
+        exitSystemButton.addActionListener(e -> {
+            System.exit(0);
+        });
+    }
+
+    public void menuRun() {
+        frame = new JFrame("Menu");
+        frame.setContentPane(this.root);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
