7916a95099a3bbe7b2d7727a8df966601ef6e6d9
zzwtsy <58852496+zzwtsy@users.noreply.github.com>
2022-06-10 13:10:52 +0800

    打开注册界面时检查管理员是否已注册判断配置文件是否存在

--- src/hwk1/GUI/Register.java
+++ src/hwk1/GUI/Register.java
@@ -1,71 +1,98 @@
 package hwk1.GUI;
 
+import com.google.gson.Gson;
 import hwk1.MyJson;
 import org.json.JSONObject;
 
 import javax.swing.*;
 import java.awt.*;
 import java.io.File;
+import java.util.Map;
 
 public class Register {
+    static String userNameJson;
+    static String userPwdJson;
+    private final JFrame frame = new JFrame("register");
+    String registerUserName;
+    String registerUserPwd1;
+    String registerUserPwd2;
     private JTextField registerName;
     private JPasswordField registerPasswordField1;
     private JPasswordField registerPasswordField2;
-    private JFrame frame;
     private JPanel root;
     private JButton registerButton;
 
     public Register() {
         registerButton.addActionListener(e -> {
-            String registerUserName = registerName.getText();
-            String registerUserPwd1 = String.valueOf(registerPasswordField1.getPassword());
-            String registerUserPwd2 = String.valueOf(registerPasswordField2.getPassword());
+            registerUserName = registerName.getText();
+            registerUserPwd1 = String.valueOf(registerPasswordField1.getPassword());
+            registerUserPwd2 = String.valueOf(registerPasswordField2.getPassword());
             //用户点击注册时判断用户名是否为空，trim：删除前导和尾随空格
             if (registerUserName.trim().equals("")) {
                 JOptionPane.showMessageDialog(null, "用户名不能为空");
-            }
-            //用户点击注册时判断密码1是否为空，trim：删除前导和尾随空格
-            if (registerUserPwd1.trim().equals("")) {
+                //用户点击注册时判断密码1是否为空，trim：删除前导和尾随空格
+            } else if (registerUserPwd1.trim().equals("")) {
                 JOptionPane.showMessageDialog(null, "密码不能为空");
-            }
-            //用户点击注册时判断密码2是否为空，trim：删除前导和尾随空格
-            if (registerUserPwd2.trim().equals("")) {
+                //用户点击注册时判断密码2是否为空，trim：删除前导和尾随空格
+            } else if (registerUserPwd2.trim().equals("")) {
                 JOptionPane.showMessageDialog(null, "密码不能为空");
-            }
-            //用户点击注册时判断密码1与密码2是否一致，trim：删除前导和尾随空格
-            if (!registerUserPwd1.equals(registerUserPwd2)) {
+                //用户点击注册时判断密码1与密码2是否一致，trim：删除前导和尾随空格
+            } else if (!registerUserPwd1.equals(registerUserPwd2)) {
                 JOptionPane.showMessageDialog(null, "两次密码不一致");
-            }
-            //当用户名密码都正确填写时进入登录界面
-            if (!registerUserName.trim().equals("") && registerUserPwd1.equals(registerUserPwd2)) {
+            } else {
                 JSONObject jsonObject = new JSONObject();
                 jsonObject.put("userName", registerUserName);
                 jsonObject.put("userPwd", registerUserPwd1);
+                File jsonFile = new File("config.json");
                 try {
-                    File jsonFile = new File("config.json");
-                    if (!jsonFile.exists()) {
-                        jsonFile.createNewFile();
-                    }else{
-                        MyJson.writeJson(jsonObject, jsonFile);
-                    }
+                    MyJson.writeJson(jsonObject, jsonFile);
+                    toLoginFrame(registerUserName, registerUserPwd1);
                 } catch (Exception ex) {
                     ex.printStackTrace();
                 }
-                //关闭注册窗口
-                frame.dispose();
-                Login login = new Login();
-                login.loginRun(registerUserName, registerUserPwd1);
             }
         });
     }
 
     public static void main(String[] args) {
         Register register = new Register();
-        register.registerRun();
+        File jsonFile = new File("config.json");
+        //检查config.json文件是否存在
+        if (jsonFile.exists()) {
+            //判断管理员是否注册
+            if (register.unregistered()) {
+                register.toLoginFrame(userNameJson, userPwdJson);
+            } else {
+                register.registerRun();
+            }
+        } else {
+            register.registerRun();
+        }
+    }
+
+    /**
+     * 判断管理员是否注册
+     */
+    public boolean unregistered() {
+        String contentJson = MyJson.readJson("config.json");
+        Gson gson = new Gson();
+        Map<String, String> mapJson = gson.fromJson(contentJson, Map.class);
+        userNameJson = mapJson.get("userName");
+        userPwdJson = mapJson.get("userPwd");
+        return userNameJson != null && userPwdJson != null;
+    }
+
+    /**
+     * 跳转登录窗口
+     */
+    public void toLoginFrame(String userNameJson, String userPwdJson) {
+        //关闭注册窗口
+        frame.dispose();
+        Login login = new Login();
+        login.loginRun(userNameJson, userPwdJson);
     }
 
     public void registerRun() {
-        frame = new JFrame("register");
         frame.setContentPane(this.root);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
