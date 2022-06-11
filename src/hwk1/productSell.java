edd9b31b9f0c6b6c1c1483d8b740481f94eae867
zzwtsy <m3226257319@gmail.com>
2022-06-11 16:20:29 +0800

    完善付款方法

--- src/hwk1/productSell.java
+++ src/hwk1/productSell.java
@@ -1,35 +1,17 @@
 package hwk1;
 
-import java.util.Date;
-
 public class productSell {
-//    public void pay()  {
-//        double count;
-//        //String类型转换int型。计算用户应付款金额
-//        double commodityPrices = Double.parseDouble(namePrice[drinksSerialNumber][1]) * buyNumber;
-//        System.out.print("请投币" + commodityPrices + "元:(输入0中止付款)");
-//        //接收用户投币
-//        userPayMoney = sc.nextDouble();
-//        if (userPayMoney == 0) {
-//            test();
-//        }
-//        while (true) {
-//            if (userPayMoney > commodityPrices) {
-//                System.out.println("正在为您找零" + (userPayMoney - commodityPrices) + "元");
-//                System.out.println("请取走您的饮料,欢迎下次光临。");
-//                break;
-//            } else if (userPayMoney - commodityPrices == 0) {
-//                System.out.println("请取走您的饮料,欢迎下次光临。");
-//                break;
-//            } else {
-//                System.out.print("投币不足!!!" + "\n" + "请继续投币" + (commodityPrices - userPayMoney) + "元(输入0中止付款):");
-//                count = sc.nextDouble();
-//                if (count == 0) {
-//                    test();
-//                }
-//                userPayMoney += count;
-//            }
-//        }
+    public float pay(float userPayMoney, int productNumbers) {
+        double count;
+        //计算用户应付款金额
+        float accountsPayable = userPayMoney * productNumbers;
+        if (userPayMoney > accountsPayable) {
+            return (userPayMoney - accountsPayable);
+        } else if (userPayMoney - accountsPayable == 0) {
+            return 0;
+        } else {
+            return -1;
+        }
 //        number[drinksSerialNumber] = number[drinksSerialNumber] - buyNumber;
 //        SALE_TIME[logCount][0] = DATE.format(new Date());
 //        logCount += 1;
@@ -46,5 +28,5 @@ public class productSell {
 //                "元\n找零:\t" + (userPayMoney - commodityPrices) +
 //                "元\n购买日期:" + SALE_TIME[logCount - 1][0]);
 //        salesInterface();//返回售卖界面
-//    }
+    }
 }
