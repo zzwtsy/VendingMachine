770e889b4a5fa06fd5e81550cdb50b1a186db60f
zzwtsy <m3226257319@gmail.com>
2022-06-11 11:25:15 +0800

    添加售卖产品方法

---
+++ src/hwk1/productSell.java
@@ -0,0 +1,50 @@
+package hwk1;
+
+import java.util.Date;
+
+public class productSell {
+//    public void pay()  {
+//        double count;
+//        //String类型转换int型。计算用户应付款金额
+//        double commodityPrices = Double.parseDouble(namePrice[drinksSerialNumber][1]) * buyNumber;
+//        System.out.print("请投币" + commodityPrices + "元:(输入0中止付款)");
+//        //接收用户投币
+//        userPayMoney = sc.nextDouble();
+//        if (userPayMoney == 0) {
+//            test();
+//        }
+//        while (true) {
+//            if (userPayMoney > commodityPrices) {
+//                System.out.println("正在为您找零" + (userPayMoney - commodityPrices) + "元");
+//                System.out.println("请取走您的饮料,欢迎下次光临。");
+//                break;
+//            } else if (userPayMoney - commodityPrices == 0) {
+//                System.out.println("请取走您的饮料,欢迎下次光临。");
+//                break;
+//            } else {
+//                System.out.print("投币不足!!!" + "\n" + "请继续投币" + (commodityPrices - userPayMoney) + "元(输入0中止付款):");
+//                count = sc.nextDouble();
+//                if (count == 0) {
+//                    test();
+//                }
+//                userPayMoney += count;
+//            }
+//        }
+//        number[drinksSerialNumber] = number[drinksSerialNumber] - buyNumber;
+//        SALE_TIME[logCount][0] = DATE.format(new Date());
+//        logCount += 1;
+//        saleStatus = "成功";
+//        writeLog();
+//        //找零日志记录
+//        LOG_CHANGE[logCount][0] = (userPayMoney - commodityPrices);
+//        System.out.println("***购买小票***");
+//        //打印购买小票
+//        System.out.println("商品名称:" + namePrice[drinksSerialNumber][0] +
+//                "\n单价:\t" + namePrice[drinksSerialNumber][1] +
+//                "元\n数量:\t" + buyNumber +
+//                "瓶\n总价:\t" + commodityPrices +
+//                "元\n找零:\t" + (userPayMoney - commodityPrices) +
+//                "元\n购买日期:" + SALE_TIME[logCount - 1][0]);
+//        salesInterface();//返回售卖界面
+//    }
+}
