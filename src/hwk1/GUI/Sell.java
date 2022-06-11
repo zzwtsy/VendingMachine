50170f6f9a59adb996e294516abafa6e680f4b36
zzwtsy <m3226257319@gmail.com>
2022-06-11 10:24:26 +0800

    添加售卖界面

---
+++ src/hwk1/GUI/Sell.java
@@ -0,0 +1,53 @@
+package hwk1.GUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Sell {
+    private JPanel root;
+    private JLabel textHeader;
+    private JPanel showPanel;
+    private JPanel choicePanel;
+    private JButton 购买Button;
+    private JTextField textField1;
+    private JList serialNumberList;
+    private JList nameList;
+    private JList priceList;
+    private JPanel serialNumberPanel;
+    private JList numberList;
+    private JPanel namePanel;
+    private JPanel pricePanel;
+    private JPanel numberPanel;
+    private void setWindowText(){
+        textHeader.setText("自动售卖机");
+    }
+
+    public static void main(String[] args) {
+        new Sell().sellRun();
+    }
+
+    public void sellRun() {
+        JFrame frame = new JFrame("Sell");
+        frame.setContentPane(new Sell().root);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        //设置窗口文字
+        setWindowText();
+        frame.setVisible(true);
+        // 获得窗口宽
+        int windowWidth = frame.getWidth();
+        // 获得窗口高
+        int windowHeight = frame.getHeight();
+        // 定义工具包
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        // 获取屏幕的尺寸
+        Dimension screenSize = kit.getScreenSize();
+        // 获取屏幕的宽
+        int screenWidth = screenSize.width;
+        // 获取屏幕的高
+        int screenHeight = screenSize.height;
+        // 设置窗口居中显示
+        frame.setLocation(screenWidth / 2 - windowWidth / 2, screenHeight / 2 - windowHeight / 2);
+    }
+}
