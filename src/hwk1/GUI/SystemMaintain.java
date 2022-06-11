73ab017880bfe78cfb7edbbe50f3554ce391b663
zzwtsy <m3226257319@gmail.com>
2022-06-11 08:12:27 +0800

    添加系统维护界面

---
+++ src/hwk1/GUI/SystemMaintain.java
@@ -0,0 +1,57 @@
+package hwk1.GUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SystemMaintain {
+    private JFrame frame;
+    private JPanel root;
+    private JLabel text1;
+    private JButton productControlButton;
+    private JButton modifyUserPwdButton;
+    private JButton displayLogButton;
+    private JButton goBackButton;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("SystemMaintain");
+        frame.setContentPane(new SystemMaintain().root);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        // 获得窗口宽
+        int windowWidth = frame.getWidth();
+        // 获得窗口高
+        int windowHeight = frame.getHeight();
+        // 定义工具包
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        // 获取屏幕的尺寸
+        Dimension screenSize = kit.getScreenSize();
+        // 获取屏幕的宽
+        int screenWidth = screenSize.width;
+        // 获取屏幕的高
+        int screenHeight = screenSize.height;
+        // 设置窗口居中显示
+        frame.setLocation(screenWidth / 2 - windowWidth / 2, screenHeight / 2 - windowHeight / 2);
+    }
+    public void SystemMaintainRun(){
+        frame = new JFrame("SystemMaintain");
+        frame.setContentPane(this.root);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        // 获得窗口宽
+        int windowWidth = frame.getWidth();
+        // 获得窗口高
+        int windowHeight = frame.getHeight();
+        // 定义工具包
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        // 获取屏幕的尺寸
+        Dimension screenSize = kit.getScreenSize();
+        // 获取屏幕的宽
+        int screenWidth = screenSize.width;
+        // 获取屏幕的高
+        int screenHeight = screenSize.height;
+        // 设置窗口居中显示
+        frame.setLocation(screenWidth / 2 - windowWidth / 2, screenHeight / 2 - windowHeight / 2);
+    }
+}
