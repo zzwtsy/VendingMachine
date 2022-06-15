6e6779bc26ec73bdb0b59b15e39c88d8739a81c9
zzwtsy <58852496+zzwtsy@users.noreply.github.com>
2022-06-15 15:53:59 +0800

    添加修改产品信息方法

--- src/hwk/utils/UpdateProductInfo.java
+++ src/hwk/utils/UpdateProductInfo.java
@@ -6,11 +6,12 @@ import java.io.File;
 
 public class UpdateProductInfo {
     /**
+     * 更新商品数量
      *
      * @param n 用户选择的商品序号
      * @param j 用户选择的商品数量
      */
-    public static void updateProductInfo(int n, int j) {
+    public static void updateProductNumbers(int n, int j) {
         GetProductInfo getProductInfo = new GetProductInfo();
         String[][] data = getProductInfo.getProductInfoData(getProductInfo.getProductInfoJson());
         JSONObject json = new JSONObject();
@@ -38,4 +39,41 @@ public class UpdateProductInfo {
         }
     }
 
+    /**
+     *
+     * @param n 用户选择的商品序号
+     * @param productName 用户修改的商品名
+     * @param productPrice 用户修改的商品价格
+     * @param productNumbers 用户修改的商品数量
+     */
+    public static void modifyInfo(int n, String productName, String productPrice, String productNumbers) {
+        GetProductInfo getProductInfo = new GetProductInfo();
+        String[][] data = getProductInfo.getProductInfoData(getProductInfo.getProductInfoJson());
+        JSONObject json = new JSONObject();
+        for (int i = 0; i < data.length; i++) {
+            //jsonObject = null 不可删除
+            @SuppressWarnings("all") JSONObject jsonObject = null;
+
+            jsonObject = new JSONObject();
+            //detail = null 不可删除
+            @SuppressWarnings("all") String[] detail = null;
+            //如果i=n 商品数量减一
+            if (n == i) {
+                jsonObject.put("productName", productName);
+                jsonObject.put("productPrice", productPrice);
+                jsonObject.put("productNumbers", productNumbers);
+            } else {
+                jsonObject.put("productName", data[i][1]);
+                jsonObject.put("productPrice", data[i][2]);
+                jsonObject.put("productNumbers", data[i][3]);
+            }
+            json.put(String.valueOf(i), jsonObject);
+        }
+        File jsonFile = new File("product.json");
+        try {
+            MyJson.writeJson(json, jsonFile);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
