2a50ebd97598fc7acf026f8f0fff8b5419a2025b
zzwtsy <58852496+zzwtsy@users.noreply.github.com>
2022-06-16 00:37:19 +0800

    添加修改管理员密码代码

---
+++ src/hwk/gui/maintain/product_control/ModifyUserInfo.java
@@ -0,0 +1,77 @@
+package hwk.gui.maintain.product_control;
+
+import hwk.gui.maintain.SystemMaintain;
+import hwk.utils.CheckRegistered;
+import hwk.utils.MyJson;
+import org.json.JSONObject;
+
+import javax.swing.*;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+
+import static hwk.utils.WindowCenter.initFrame;
+
+public class ModifyUserInfo {
+    private JFrame frame;
+    private JPasswordField userPwdField1;
+    private JPasswordField userPwdField2;
+    private JPanel root;
+    private JLabel userPwdLabel1;
+    private JLabel userPwdLabel2;
+    private JButton okButton;
+    private JButton cancelButton;
+
+    public ModifyUserInfo() {
+        okButton.addActionListener(e -> {
+            CheckRegistered checkRegistered = new CheckRegistered();
+            checkRegistered.checkRegistered();
+            String userName = checkRegistered.getUserNameJson();
+            String userPwd1 = String.valueOf(userPwdField1.getPassword());
+            String userPwd2 = String.valueOf(userPwdField2.getPassword());
+            if (userPwd1.equals(userPwd2)) {
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("userName", userName);
+                jsonObject.put("userPwd", userPwd1);
+                File jsonFile = new File("config.json");
+                try {
+                    MyJson.writeJson(jsonObject, jsonFile);
+                    JOptionPane.showMessageDialog(null, "修改成功");
+                    frame.dispose();
+                    new SystemMaintain().SystemMaintainRun();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        cancelButton.addActionListener(e -> {
+            frame.dispose();
+            new SystemMaintain().SystemMaintainRun();
+        });
+    }
+
+    private void setWindowText() {
+        userPwdLabel1.setText("修改密码");
+        userPwdLabel2.setText("请再次输入密码");
+        okButton.setText("修改");
+        cancelButton.setText("取消");
+    }
+
+    public void modifyUserInfoRun() {
+        frame = new JFrame("ModifyUserInfo");
+        frame.setContentPane(this.root);
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                frame.dispose();
+                new SystemMaintain().SystemMaintainRun();
+            }
+        });
+        frame.pack();
+        frame.setVisible(true);
+        //设置窗口文字
+        setWindowText();
+        //窗口居中
+        initFrame(frame);
+    }
+}
